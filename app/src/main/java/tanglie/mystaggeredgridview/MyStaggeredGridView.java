a84a5b3c30922de7f771376fe9b101c7e895d9ca
tanglie1993 <tanglie23@163.com>
2016-09-03 23:24:00 +0800

    实现根据columncount调整宽度

--- app/src/main/java/tanglie/mystaggeredgridview/MyStaggeredGridView.java
+++ app/src/main/java/tanglie/mystaggeredgridview/MyStaggeredGridView.java
@@ -1,6 +1,7 @@
 package tanglie.mystaggeredgridview;
 
 import android.content.Context;
+import android.icu.util.Measure;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,40 +15,46 @@ import java.lang.Override;
 /**
  * Created by Administrator on 2016/9/3 0003.
  */
-public class MyStaggeredGridView extends LinearLayout {
+public class MyStaggeredGridView extends ViewGroup {
 
     private ListAdapter adapter;
+    private int columnCount = 10;
 
     public MyStaggeredGridView(Context context) {
         super(context);
-        setOrientation(VERTICAL);
+//        setOrientation(VERTICAL);
     }
 
     public MyStaggeredGridView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setOrientation(VERTICAL);
+//        setOrientation(VERTICAL);
     }
 
     public MyStaggeredGridView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        setOrientation(VERTICAL);
+//        setOrientation(VERTICAL);
     }
 
     public MyStaggeredGridView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        setOrientation(VERTICAL);
+//        setOrientation(VERTICAL);
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int widthSpecSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        int columnMaxWidth = widthSpecSize / columnCount;
         for(int i = 0; i < getChildCount(); i++){
             View view = getChildAt(i);
-            ViewGroup.LayoutParams params = view.getLayoutParams();
-            params.height = ViewGroup.LayoutParams.WRAP_CONTENT;
-            params.width = ViewGroup.LayoutParams.WRAP_CONTENT;
-            view.setLayoutParams(params);
             view.measure(0, 0);
+            if(view.getMeasuredWidth() > columnMaxWidth){
+                float scaleFactor = (float) columnMaxWidth / (float) view.getMeasuredWidth();
+                int newWidthMeasureSpec = MeasureSpec.makeMeasureSpec(columnMaxWidth, MeasureSpec.EXACTLY);
+                int newHeightMeasureSpec = MeasureSpec.makeMeasureSpec((int) (scaleFactor * view.getMeasuredHeight()), MeasureSpec.EXACTLY);
+                view.measure(newWidthMeasureSpec, newHeightMeasureSpec);
+            }
         }
     }
 
